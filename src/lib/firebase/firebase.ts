83892ccb4940189cad64ef552f46c8f04f5fa094
abeken1999 <41886370+abeken1999@users.noreply.github.com>
2023-04-20 00:31:12 +0900

    Update firebase.ts

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -8,7 +8,7 @@ import {
   FIREBASE_STORAGE_BUCKET,
 } from '@/constant/env'
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
   authDomain: FIREBASE_AUTH_DOMAIN,
   projectId: FIREBASE_PROJECT_ID,
